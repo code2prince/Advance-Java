2ea3ea9d83e059cdae5714b188c7558f3f1ad0d6
github_username <princekumarsharma365@gmail.com>
2026-07-20 23:36:59 +0530

    check anagram approach 2

--- src/Practice/JavaConcept/CheckAnagram.java
+++ src/Practice/JavaConcept/CheckAnagram.java
@@ -7,6 +7,10 @@ public class CheckAnagram {
         String str1="listen";
         String str2="silent";
 
+        if(str1.length()!=str2.length()){
+            System.out.println(false);
+        }
+
         char[] a=str1.toCharArray();
         char[] b=str2.toCharArray();
 
@@ -17,6 +21,32 @@ public class CheckAnagram {
         System.out.println(b);
 
         System.out.println(Arrays.equals(a,b));
+
+        //----------------Approach 2-------------------------------------------
+
+        if(str1.length()!=str2.length()){
+            System.out.println(false);
+        }
+
+        int[] count=new int[26];
+        for(int i=0; i<str1.length(); i++){
+            count[str1.charAt(i)-'a']++;    //--here 'l'-'a' =12 at 12 index in count array mark 1
+            count[str2.charAt(i)-'a']--;
+        }
+
+        for(int i: count){
+        System.out.print(i+" ");
+            if(i!=0){
+                System.out.println(false);
+            }
+        }
+        System.out.println("anagram");
+
+
+
+
+
+
 
 
     }
